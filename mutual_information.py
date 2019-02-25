c6ea2965ab4e9ad89396e8a58b6d20c278fcfbd0
Diego Alcantara <dalcantara7@email.arizona.edu>
2019-02-25 15:50:57 -0700

    try this

--- mutual_information.py
+++ mutual_information.py
@@ -94,35 +94,25 @@ feature 3 MI score vector is [MIfeature1,MIfeature2,MIfeature4...MIfeature6]
 
 """
 
-data = np.loadtxt(r'C:\Users\andre\Desktop\Repo498\fractal-eyes\full_image_set_analysis.csv',
-                  delimiter = ',')  
-dataframe = pd.DataFrame(data, columns = ['feature0','feature1','feature2','feature3',
-                                          'feature4','feature5','labels'])
-MI_1 = mutualInformationFeatures(dataframe,'feature0','labels')
-print(MI_1)
+# data = np.loadtxt(r'C:\Users\andre\Desktop\Repo498\fractal-eyes\full_image_set_analysis.csv',
+#                   delimiter = ',')  
+# dataframe = pd.DataFrame(data, columns = ['feature0','feature1','feature2','feature3',
+#                                           'feature4','feature5','labels'])
+# MI_1 = mutualInformationFeatures(dataframe,'feature0','labels')
+# print(MI_1)
 
-MI_2 = mutualInformationFeatures(dataframe,'feature1','labels')
-print(MI_2)
-MI_3 = mutualInformationFeatures(dataframe,'feature2','labels')
-print(MI_3)
-MI_4 = mutualInformationFeatures(dataframe,'feature3','labels')
-print(MI_4)
-MI_5 = mutualInformationFeatures(dataframe,'feature4','labels')
-print(MI_5)
-MI_6 = mutualInformationFeatures(dataframe,'feature5','labels')
-print(MI_6)
+# MI_2 = mutualInformationFeatures(dataframe,'feature1','labels')
+# print(MI_2)
+# MI_3 = mutualInformationFeatures(dataframe,'feature2','labels')
+# print(MI_3)
+# MI_4 = mutualInformationFeatures(dataframe,'feature3','labels')
+# print(MI_4)
+# MI_5 = mutualInformationFeatures(dataframe,'feature4','labels')
+# print(MI_5)
+# MI_6 = mutualInformationFeatures(dataframe,'feature5','labels')
+# print(MI_6)
 
-final = np.column_stack((MI_1,MI_2,MI_3,MI_4,MI_5,MI_6))
+# final = np.column_stack((MI_1,MI_2,MI_3,MI_4,MI_5,MI_6))
 
-saveFile = r'C:\Users\andre\Desktop\Repo498\fractal-eyes\Mutual_Information_Features.csv'
-np.savetxt(saveFile,final,delimiter = ',')
-
-
-
-    
-    
-    
-    
-    
-    
-    
+# saveFile = r'C:\Users\andre\Desktop\Repo498\fractal-eyes\Mutual_Information_Features.csv'
+# np.savetxt(saveFile,final,delimiter = ',')
